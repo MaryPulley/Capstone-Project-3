509133058fd25d6e2623e41cf45386f2b2b6dfe5
Asa Adomatis <asaa@clemson.edu>
2025-04-01 20:59:15 -0400

    possibly finished local storage version, but also runs out of space

--- preprocessing.py
+++ preprocessing.py
@@ -20,10 +20,10 @@ def _import_images(image_paths) -> list[Image]:
 
     return images, errors
 
-def get_images_and_df():
+def get_images_and_df(path=None):
     """function that gets the X variable in the proper format from the datset"""
-
-    path = kh.dataset_download("mostafaabla/garbage-classification")
+    if path is None:
+        path = kh.dataset_download("mostafaabla/garbage-classification")
     file_dict = {"file_names": [], "file_paths": [], "classification": []}
     for root, _, files in os.walk(path):
         for file in files:
@@ -42,18 +42,18 @@ def get_images_and_df():
 
     return images, df
 
-def get_X(images, verbose=False):
+def get_X(images, verbose=False, target_size=None):
     """function that gets the X variable in the proper format given a set of unprocessed images"""
 
-    
-    sizes = set([img.size for img in images])
-    x_size = [shape[0] for shape in sizes]
-    y_size = [shape[1] for shape in sizes]
+    if target_size is None:
+        sizes = set([img.size for img in images])
+        x_size = [shape[0] for shape in sizes]
+        y_size = [shape[1] for shape in sizes]
 
-    # get the median x and y size
-    x_size = int(np.median(x_size))
-    y_size = int(np.median(y_size))
-    target_size = (x_size, y_size)
+        # get the median x and y size
+        x_size = int(np.median(x_size))
+        y_size = int(np.median(y_size))
+        target_size = (x_size, y_size)
 
     # Create a temporary file to store the processed images
     temp_file = tempfile.NamedTemporaryFile(delete=False)
@@ -70,11 +70,20 @@ def get_X(images, verbose=False):
     processed_imgs = np.memmap(temp_file.name, dtype=np.float32, mode='r+', shape=processed_imgs_shape)
 
     for i, img in enumerate(images):
-        print(img.shape)
+        # Convert grayscale images to RGB
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+            if verbose:
+                print(f"Image {i} required to be converted to RGB")
         processed_img = img.resize(target_size, resample=Image.LANCZOS)
         processed_img = np.array(processed_img).astype(np.float32)
+        print("processed img shape", processed_img.shape)
         processed_img = processed_img / 255.0
+        print("processed img shape post refinement", processed_img.shape)
         processed_imgs[i] = processed_img
+
+    # Delete the temporary file
+    os.remove(temp_file.name)
 
     return processed_imgs
 
